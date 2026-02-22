a8e2363ad136945cf776dfa4f0c1d647cba9db51
Roudi Hannan <roudihannan7@gmail.com>
2026-02-23 00:12:39 +0100

    feat: setup basic express server with typescript and middleware

---
+++ back/src/index.ts
@@ -0,0 +1,28 @@
+import express, {Application, Request, Response} from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+
+app.get('/health', (req: Request, res: Response) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'Server is running perfectly'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is flying on: http:localhost:${PORT}`);
+});
