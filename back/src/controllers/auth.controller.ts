f21241e30fad307ebe623a8922301aec2f28f27a
Roudi Hannan <roudihannan7@gmail.com>
2026-02-23 01:32:44 +0100

    feat: implement login with JWT and secure HttpOnly cookie

--- back/src/controllers/auth.controller.ts
+++ back/src/controllers/auth.controller.ts
@@ -1,22 +1,20 @@
 import {Request, Response} from 'express';
 import {User} from '../models/user.model';
 import bcrypt from 'bcryptjs';
+import {generateToken} from '../utils/generateToken';
 
 export const register = async (req: Request, res: Response) => {
     try {
         const {name, email, password} = req.body;
 
-        // 1. if user already exists
         const userExists = await User.findOne({email});
         if(userExists) {
             return res.status(400).json({message: 'User already exists'});
         }
 
-        // 2. hashing password
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
-        // 3. create user and save
         const user = await User.create({
             name,
             email,
@@ -39,3 +37,33 @@ export const register = async (req: Request, res: Response) => {
     }
 };
 
+export const login = async (req: Request, res: Response) => {
+    try {
+        const {email, password} = req.body;
+
+        const user = await User.findOne({email}).select('+password');
+
+        if(user && (await bcrypt.compare(password, user.password))) {
+
+            generateToken(res, user._id.toString());
+
+            res.status(200).json({
+                message: 'Logged in successfully',
+                user: {
+                    id: user._id,
+                    name: user.name,
+                    email: user.email
+                }
+            });
+        }else{
+            res.status(401).json({
+                message: 'Invalid email or password'
+            });
+        }
+    }catch(error) {
+        res.status(500).json({
+            message: 'Server Error',
+            error: (error as Error).message
+        });
+    }
+};
